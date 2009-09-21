6eb1d6cc9aabda4f7d391c135b4db583c5488c32
Derick Bailey <derickbailey@gmail.com>
2009-09-21 15:48:47 -0500

    adding patches folder for the specs, and moving the system patch into there, renaming from msbuildfake.

--- lib/spec/msbuild_spec.rb
+++ lib/spec/msbuild_spec.rb
@@ -1,7 +1,7 @@
 require File.join(File.expand_path(File.dirname(__FILE__)), 'support', 'spec_helper')
 require 'msbuild'
 require 'msbuildtestdata'
-require 'msbuildfake'
+require 'system_patch'
 
 describe MSBuild, "when an msbuild path is not specified" do
 	
--- lib/spec/support/spec_helper.rb
+++ lib/spec/support/spec_helper.rb
@@ -1,3 +1,4 @@
 $: << File.join(File.expand_path(File.dirname(__FILE__)), "../")
 $: << File.join(File.expand_path(File.dirname(__FILE__)), "../../")
 $: << File.join(File.expand_path(File.dirname(__FILE__)), "../model/")
+$: << File.join(File.expand_path(File.dirname(__FILE__)), "../patches/")
