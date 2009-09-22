782cc76c3a5093cc8c6d808546972c565fb6acba
Derick Bailey <derickbailey@gmail.com>
2009-09-21 22:07:36 -0500

    adding empty spec/observation to remind me that i need to start working on the assembly info generator / task

---
+++ lib/spec/assemblyinfo_spec.rb
@@ -0,0 +1,5 @@
+require File.join(File.expand_path(File.dirname(__FILE__)), 'support', 'spec_helper')
+
+describe "when generating an assembly info file" do
+	it "should contain the specified version information"
+end
