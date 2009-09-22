17b09cccb3b8b3e1354afbefcc4c3107ece6d73b
Derick Bailey <derickbailey@gmail.com>
2009-09-21 20:53:51 -0500

    replacing the array parameter building with a simple join

--- lib/patches/buildparameters.rb
+++ lib/patches/buildparameters.rb
@@ -10,10 +10,6 @@ end
 
 module ArrayParameterBuilder
 	def build_parameters
-		option_text = ''
-		self.each do |value|
-			option_text << "#{value};"
-		end
-		option_text.chop
+		self.join ";"
 	end
 end
