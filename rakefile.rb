a073d1b42d902f24b776546d4c9bd5386fefe4c6
Derick Bailey <derickbailey@gmail.com>
2009-09-22 12:05:27 -0500

    moved the specs rake task outside of the albacore namespace

--- rakefile.rb
+++ rakefile.rb
@@ -1,19 +1,19 @@
 require 'spec/rake/spectask'
 require 'lib/albacore'
 
-task :default => 'albacore:specs'
+task :default => :specs
+
+desc "Run functional specs for the Albacore framework"
+Spec::Rake::SpecTask.new :specs do |t|
+	t.spec_opts << '--colour'
+	t.spec_opts << '--format specdoc'
+	t.spec_files = FileList['lib/spec/**/*_spec.rb']
+end
 
 namespace :albacore do
 	
 	task :sample => ['albacore:assemblyinfo', 'albacore:msbuild']
 	
-	desc "Run functional specs for the Albacore framework"
-	Spec::Rake::SpecTask.new :specs do |t|
-		t.spec_opts << '--colour'
-		t.spec_opts << '--format specdoc'
-		t.spec_files = FileList['lib/spec/**/*_spec.rb']
-	end
-
 	desc "Run a sample build using the MSBuildTask"
 	Rake::MSBuildTask.new(:msbuild) do |msb|
 		msb.properties = {:configuration => :Debug}
