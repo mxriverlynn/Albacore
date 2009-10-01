4ee7a7691c9ca0ea034b52b3c1bd788a74b2adec
Derick Bailey <derickbailey@gmail.com>
2009-09-30 22:09:35 -0500

    ignoring coverage files, and adjusting rake file to use working directory for ncover console task

--- lib/spec/support/AssemblyInfo/AssemblyInfo.cs
+++ lib/spec/support/AssemblyInfo/AssemblyInfo.cs
@@ -1,13 +1,4 @@
 using System.Reflection;
 using System.Runtime.InteropServices;
 
-[assembly: AssemblyTitle("my assembly title")]
-[assembly: AssemblyDescription("this is the assembly description")]
-[assembly: AssemblyCompany("a test company")]
-[assembly: AssemblyProduct("a product name goes here")]
-[assembly: AssemblyCopyright("copyright some year, by some legal entity")]
-[assembly: AssemblyVersion("0.1.2.3")]
-
-[assembly: AnotherAttribute("with some data")]
-[assembly: SomeAttribute("some value goes here")]
 
