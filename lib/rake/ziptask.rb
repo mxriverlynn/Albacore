77b15ec70d4219638eed9c73450a02c5a34602a5
Ben Hall <Ben@Benhall.me.uk>
2009-10-15 15:20:08 +0100

    Added a zip task

---
+++ lib/rake/ziptask.rb
@@ -0,0 +1,21 @@
+require 'rake/tasklib'
+require 'zipdirectory'
+
+module Albacore
+	class ZipTask < Rake::TaskLib
+		attr_accessor :name
+		
+		def initialize(name=:zip)
+			@name = name
+			@zip = ZipDirectory.new
+			yield @zip if block_given?
+			define
+		end
+		
+		def define
+			task name do
+				@zip.package
+			end
+		end		
+	end
+end
