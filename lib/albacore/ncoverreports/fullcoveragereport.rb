ba12dbcfe9c66634825484f24d432cd162ca8869
Derick Bailey <derickbailey@gmail.com>
2009-10-19 13:40:40 -0500

    added basic spec and code to run ncover report and create a full coverage html report

---
+++ lib/albacore/ncoverreports/fullcoveragereport.rb
@@ -0,0 +1,25 @@
+require File.join(File.dirname(__FILE__), '../', 'support', 'albacore_helper')
+
+module NCover
+	module Reports
+		
+		class FullCoverage
+			include YAMLConfig
+			
+			attr_accessor :output_path
+			
+			def initialize
+				super()
+			end
+			
+			def report_type
+				:FullCoverageReport
+			end
+			
+			def report_format
+				:Html
+			end
+		end
+		
+	end
+end
