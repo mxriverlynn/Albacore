78866af6b6bf768fc126099412e42393f77aa55a
Derick Bailey <derickbailey@gmail.com>
2009-10-24 13:33:08 -0500

    adjust sftp to allow multiple file uploads

--- lib/albacore/sftp.rb
+++ lib/albacore/sftp.rb
@@ -5,15 +5,20 @@ class Sftp
 	include YAMLConfig
 	include Logging
 	
-	attr_accessor :server, :username, :password, :local_file, :remote_file
+	attr_accessor :server, :username, :password, :files_to_upload
 	
 	def initialize
 		super()
+		@files_to_upload = {}
 	end	
 	
 	def upload()
 		Net::SFTP.start(@server, @username, :password => @password) do |sftp|
-			sftp.upload!(@local_file, @remote_file)
+			@logger.debug "Starting File Upload"
+			@files_to_upload.each {|local_file, remote_file|
+				@logger.debug "Uploading #{local_file} to #{remote_file}"
+				sftp.upload!(local_file, remote_file)
+			}
 		end
 	end
 end
--- spec/sftp_spec.rb
+++ spec/sftp_spec.rb
@@ -1,7 +1,7 @@
 require File.join(File.expand_path(File.dirname(__FILE__)), 'support', 'spec_helper')
 require 'sftp'
 
-describe Sftp, 'when uploading a file over sftp' do
+describe Sftp, 'when uploading files over sftp' do
 	before :each do
 		@sftpstub = Net::SFTP::Session.stub_instance(:upload! => nil)
 		Net::SFTP.stub_method(:start).yields(@sftpstub)
@@ -10,8 +10,11 @@ describe Sftp, 'when uploading a file over sftp' do
 		@sftp.server="server"
 		@sftp.username="user"
 		@sftp.password="secret"
-		@sftp.local_file = "some.file"
-		@sftp.remote_file = "./somefolder/some.file"
+		
+		@sftp.files_to_upload = {
+			"some.file" => "./somefolder/some.file", 
+			"another.file" => "another/folder/another.file"
+		}
 		
 		@sftp.upload
 	end
@@ -20,7 +23,8 @@ describe Sftp, 'when uploading a file over sftp' do
 		Net::SFTP.should have_received(:start).with("server", "user", :password => "secret")
 	end
 	
-	it "should upload the local file to the remote destination" do
+	it "should upload the local files to the remote destination" do
 		@sftpstub.should have_received(:upload!).with("some.file", "./somefolder/some.file")
+		@sftpstub.should have_received(:upload!).with("another.file", "another/folder/another.file")
 	end
 end
