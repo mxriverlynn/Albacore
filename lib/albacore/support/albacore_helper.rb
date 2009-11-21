cf4f290ab90cee800ace6dbf1deeae8a06d5e985
Ben Hall <ben@benhall.me.uk>
2009-11-21 17:07:37 +0000

    Fix CRLF

--- lib/albacore/support/albacore_helper.rb
+++
@@ -1,4 +0,0 @@
-require File.join(File.dirname(__FILE__), 'logging')
-require File.join(File.dirname(__FILE__), 'failure')
-require File.join(File.dirname(__FILE__), 'runcommand')
-require File.join(File.dirname(__FILE__), 'yamlconfig')
--- spec/patches/tasklib_patch.rb
+++
@@ -1,6 +0,0 @@
-module TasklibPatch
-	$task_failed = false
-	def fail
-		$task_failed = true
-	end
-end
