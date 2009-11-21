4de7cd641cf31399a326a4b42abcdee8270e307b
Ben Hall <ben@benhall.me.uk>
2009-11-21 18:45:38 +0000

    Installs the dependencies required to run specs:all

---
+++ spec/install_dependencies.rb
@@ -0,0 +1,24 @@
+require 'rubygems'
+require 'rubygems/gem_runner'
+require 'rubygems/exceptions'
+
+#http://gist.github.com/236148
+required_version = Gem::Requirement.new "> 1.8.3"
+
+unless required_version.satisfied_by? Gem.ruby_version then
+  abort "Expected Ruby Version #{required_version}, was #{Gem.ruby_version}"
+end
+
+def install(lib)
+  begin
+    Gem::GemRunner.new.run ['install', lib]
+  rescue Gem::SystemExitException => e
+  end
+end
+
+puts "Installing required dependencies"
+
+install 'net-sftp'
+install 'rubyzip'
+install 'jeweler'
+install 'derickbailey-notamock'
