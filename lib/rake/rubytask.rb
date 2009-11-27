aeaa8ac9e56fb0582223d117745b824e02d023e8
Ben Hall <Ben@Benhall.me.uk>
2009-11-27 16:05:48 +0000

    Ruby task allowing you to execute arbitary blocks of Ruby code in the block

---
+++ lib/rake/rubytask.rb
@@ -0,0 +1,19 @@
+require 'rake/tasklib'
+
+module Albacore
+	class RubyTask < Rake::TaskLib
+		attr_accessor :name
+
+		def initialize(name=:ruby, &block)
+			@name = name
+                        @block = block
+			define
+		end
+
+		def define
+			task name do
+		  	   @block.call
+			end
+		end
+	end
+end
