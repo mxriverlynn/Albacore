1524a8fe0bc74db1c405ffa4022738d43994d911
Ben Hall <Ben@Benhall.me.uk>
2009-11-27 13:27:18 +0000

    SSH can now deal with keys

--- lib/albacore/ssh.rb
+++ lib/albacore/ssh.rb
@@ -5,15 +5,26 @@ class Ssh
 	include YAMLConfig
 	include Logging
 	
-	attr_accessor :server, :username, :password, :commands
-	
+	attr_accessor :server, :username, :password, :commands, :port, :key, :debug
+  	
 	def initialize
 		super()
 		@commands = []
 	end
+  
+  	def get_connection_options
+		options = {}
+		options[:verbose] = :debug if @debug == true
+		options[:password] = @password if @password
+		options[:port] = @port if @port
+		options[:keys] = [@key] if @key
+		options
+	end
 	
 	def execute()
-		Net::SSH.start(@server, @username, :password => @password) do |ssh|
+    warn_about_key if @key
+    
+		Net::SSH.start(@server, @username, get_connection_options) do |ssh|
 			@commands.each{|cmd|
 				@logger.info "Executing remote command: #{cmd}"
 				output = ssh.exec!(cmd)
@@ -21,5 +32,11 @@ class Ssh
 				@logger.info output
 			}
 		end
-	end
+  end
+  
+  def warn_about_key()
+      info.debug 'When using a key, you need an SSH-Agent running to manage the keys.'
+      info.debug 'On Windows, a recommended agent is called Pageant, downloadable from the Putty site.'
+  end
+  
 end
