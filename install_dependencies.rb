0df8e63cdc5974abb5fe32c809eac16d3631f13e
Derick Bailey <derickbailey@gmail.com>
2009-11-30 14:03:42 -0600

    merging code to auto install dependencies, into master

---
+++ install_dependencies.rb
@@ -0,0 +1,45 @@
+require 'rubygems'
+require 'rubygems/gem_runner'
+require 'rubygems/exceptions'
+
+#http://gist.github.com/236148
+required_version = Gem::Requirement.new "> 1.8.3"
+
+unless required_version.satisfied_by? Gem.ruby_version then
+  abort "Expected Ruby Version #{required_version}, was #{Gem.ruby_version}"
+end
+
+def install(lib)
+  begin
+  	matches = Gem.source_index.find_name(lib)
+    if matches.empty?
+    	puts "Installing #{lib}"
+    	Gem::GemRunner.new.run ['install', lib]
+    else
+    	puts "Found #{lib} gem - skipping"
+    end
+  rescue Gem::SystemExitException => e
+  end
+end
+
+def add_source(url)
+  begin
+  	if Gem.sources.include?(url)
+  		puts "Found #{url} gem source = skipping"
+  	else
+  		puts "Adding #{url} gem source."
+		Gem::GemRunner.new.run ['sources', '-a', url]
+	end
+  rescue Gem::SystemExitException => e
+  end
+end
+
+puts "Installing required dependencies"
+add_source 'http://gemcutter.org'
+install 'rake'
+install 'net-ssh'
+install 'net-sftp'
+install 'rubyzip'
+install 'jeweler'
+install 'rspec'
+install 'derickbailey-notamock'
