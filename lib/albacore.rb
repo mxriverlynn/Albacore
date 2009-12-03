0b35de9def1e50f530d27904de2a66810cf4f5bf
Derick Bailey <derickbailey@gmail.com>
2009-12-03 12:40:07 -0600

    working on new task syntax and options to allow more rake like calling... Albacore::msbuild :name, [:var] => :dependency do |t,args| ... end

--- lib/albacore.rb
+++ lib/albacore.rb
@@ -5,5 +5,6 @@ $: << File.expand_path(File.join(File.dirname(__FILE__), "rake"))
 
 require 'logging'
 
+Dir.glob(File.join(File.expand_path(File.dirname(__FILE__)), 'rake/support/*.rb')).each {|f| require f }
 Dir.glob(File.join(File.expand_path(File.dirname(__FILE__)), 'rake/*.rb')).each {|f| require f }
 Dir.glob(File.join(File.expand_path(File.dirname(__FILE__)), 'albacore/*.rb')).each {|f| require f }
--- lib/rake/msbuildtask.rb
+++ lib/rake/msbuildtask.rb
@@ -1,22 +1,19 @@
 require 'rake/tasklib'
 
 module Albacore
-	class MSBuildTask < Rake::TaskLib
-		attr_accessor :name
+	
+	def self.msbuild(name=:msbuild, *args, &block)
+		MSBuildTask.new(name, *args, &block)
+	end
+	
+	class MSBuildTask < AlbacoreTask
 		
-		def initialize(name=:msbuild, &block)
-			@name = name
-			@block = block
-			define
-		end
-		
-		def define
-			task name do
-				@msbuild = MSBuild.new
-				@block.call(@msbuild) unless @block.nil?
-				@msbuild.build
-				fail if @msbuild.failed
-			end
+		def execute(task_args)
+			@msbuild = MSBuild.new
+			@block.call(@msbuild, task_args) unless @block.nil? 
+			@msbuild.build
+			fail if @msbuild.failed
 		end		
 	end
+	
 end
