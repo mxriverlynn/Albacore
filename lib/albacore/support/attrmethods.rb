6fa8edfbda66b776b01d1a43c4c848dae0c2ae04
Derick Bailey <derickbailey@gmail.com>
2010-01-10 13:40:26 -0600

    allow for metod= syntax on array and hash attributes, for backwared compatibility and to give people options

--- lib/albacore/support/attrmethods.rb
+++ lib/albacore/support/attrmethods.rb
@@ -5,12 +5,18 @@ module AttrMethods
       self.send :define_method, n do |*value|
         instance_variable_set "@#{n}", value
       end
+      self.send :define_method, "#{n}=" do |value|
+        instance_variable_set "@#{n}", value
+      end
     end
   end
   
   def attr_hash(*names)
   	names.each do |n|
       self.send :define_method, n do |value|
+        instance_variable_set "@#{n}", value
+      end
+      self.send :define_method, "#{n}=" do |value|
         instance_variable_set "@#{n}", value
       end
     end
