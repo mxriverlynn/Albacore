d7ea8a01586306739b50f86a19587a21c395fb10
Derick Bailey <derickbailey@gmail.com>
2010-01-11 10:54:25 -0600

    adjusting the xunit tests to include in xunit task specs

--- spec/support/TestSolution/TestSolution.XUnitTests/Class1.cs
+++ spec/support/TestSolution/TestSolution.XUnitTests/Class1.cs
@@ -4,16 +4,16 @@ using System.Linq;
 using System.Text;
 using Xunit;
 
-namespace TestSolution.Tests
+namespace TestSolution.XUnitTests
 {
-    public class SomeTestFixture
-    {
-        [Fact]
-        public void foo()
-        {
-            Class1 c1 = new Class1();
-            string s = c1.Foo();
-            Assert.Equal("bar", s);
-        }
-    }
+	public class SomeTestFixture
+	{
+		[Fact]
+		public void foo()
+		{
+			Class1 c1 = new Class1();
+			string s = c1.Foo();
+			Assert.Equal("bar", s);
+		}
+	}
 }
---
+++ spec/support/TestSolution/TestSolution.XUnitTests/FailingTestFixture.cs
@@ -0,0 +1,14 @@
+﻿using Xunit;
+
+namespace TestSolution.XUnitTests
+{
+	public class FailingTestFixture
+	{
+
+		[Fact]
+		public void FailingTest()
+		{
+			Assert.True(false);
+		}
+	}
+}
