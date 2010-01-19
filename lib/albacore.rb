3ae7bc0fa91f7e53a6bba1c6e03f1a4b65fd33cd
Derick Bailey <derickbailey@gmail.com>
2010-01-19 13:05:04 -0600

    added code to check for ironruby runtime and ignore ssh and sftp, to get experimental support for ironruby

--- lib/albacore.rb
+++ lib/albacore.rb
@@ -5,6 +5,14 @@ $: << File.expand_path(File.join(File.dirname(__FILE__), "rake"))
 
 require 'logging'
 
+runtime_is_ironruby = (!defined?(IRONRUBY_VERSION).nil?)
+
 Dir.glob(File.join(File.expand_path(File.dirname(__FILE__)), 'rake/support/*.rb')).each {|f| require f }
-Dir.glob(File.join(File.expand_path(File.dirname(__FILE__)), 'rake/*.rb')).each {|f| require f }
-Dir.glob(File.join(File.expand_path(File.dirname(__FILE__)), 'albacore/*.rb')).each {|f| require f }
+
+Dir.glob(File.join(File.expand_path(File.dirname(__FILE__)), 'rake/*.rb')).reject{ |f|
+  f if runtime_is_ironruby && (f.include?("ssh") || f.include?("sftp"))
+}.each {|f| require f }
+
+Dir.glob(File.join(File.expand_path(File.dirname(__FILE__)), 'albacore/*.rb')).reject{ |f|
+  f if runtime_is_ironruby && (f.include?("ssh") || f.include?("sftp"))
+}.each {|f| require f }
