99d16b085c5af954fad5308a488ddfe3b0c436d0
Derick Bailey <derickbailey@gmail.com>
2010-04-20 09:58:42 -0500

    adding specs for attr methods and allowing values to be read from the attribute when nil or empty args is passed

--- lib/albacore/support/attrmethods.rb
+++ lib/albacore/support/attrmethods.rb
@@ -3,7 +3,11 @@ module AttrMethods
   def attr_array(*names)
     names.each do |n|
       self.send :define_method, n do |*value|
-        instance_variable_set "@#{n}", value
+        if value.nil? || value.empty?
+          return instance_variable_get "@#{n}"
+        else
+          instance_variable_set "@#{n}", value
+        end
       end
       self.send :define_method, "#{n}=" do |value|
         instance_variable_set "@#{n}", value
@@ -14,7 +18,11 @@ module AttrMethods
   def attr_hash(*names)
   	names.each do |n|
       self.send :define_method, n do |value|
-        instance_variable_set "@#{n}", value
+        if value.nil? || value.empty?
+          instance_variable_get "@#{n}"
+        else
+          instance_variable_set "@#{n}", value
+        end
       end
       self.send :define_method, "#{n}=" do |value|
         instance_variable_set "@#{n}", value
---
+++ spec/attrmethods_spec.rb
@@ -0,0 +1,100 @@
+require File.join(File.dirname(__FILE__), 'support', 'spec_helper')
+require 'albacore/support/attrmethods'
+
+describe "when setting an array attribute value without the equal sign" do
+  before :each do 
+   class Test_Class
+     extend AttrMethods
+     attr_array :test
+   end 
+
+   @test = Test_Class.new
+   @test.test 1, 2, 3, 4
+  end
+
+  it "should set the array to the specified values" do
+    @test.test.count.should be(4)
+  end
+end
+
+describe "when setting an array attribute with a params list, using the equal sign" do
+  before :each do
+    class Test_Class
+      extend AttrMethods
+      attr_array :test
+    end
+
+    @test = Test_Class.new
+    @test.test = 1, 2, 3, 4
+  end
+
+  it "should set the array to the specified values" do
+    @test.test.count.should be(4)
+  end
+end
+
+describe "when setting an array attribute to an array variable using the equal sign" do
+  before :each do
+    class TestClass
+      extend AttrMethods
+      attr_array :test
+    end
+
+    test_values = [1, 2, 3, 4, 5]
+    @test = TestClass.new
+    @test.test = test_values 
+  end
+
+  it "should set the array to the values contained in the variable" do
+    @test.test.count.should be(5)
+  end
+end
+
+describe "when setting a hash attribute value without the equal sign" do
+  before :each do 
+   class Test_Class
+     extend AttrMethods
+     attr_hash :test
+   end 
+
+   @test = Test_Class.new
+   @test.test "a" => "b", "c" => "d"
+  end
+
+  it "should set the hash to the specified values" do
+    @test.test.count.should be(2)
+  end
+end
+
+describe "when setting a hash attribute with a params list, using the equal sign" do
+  before :each do
+    class Test_Class
+      extend AttrMethods
+      attr_hash :test
+    end
+
+    @test = Test_Class.new
+    @test.test = {"a" => "b", "c" => "d"}
+  end
+
+  it "should set the hash to the specified values" do
+    @test.test.count.should be(2)
+  end
+end
+
+describe "when setting a hash attribute to an array variable using the equal sign" do
+  before :each do
+    class TestClass
+      extend AttrMethods
+      attr_hash :test
+    end
+
+    test_values = {"a" => "b", "c" => "d", "e" => "f"}
+    @test = TestClass.new
+    @test.test = test_values 
+  end
+
+  it "should set the hash to the values contained in the variable" do
+    @test.test.count.should be(3)
+  end
+end
