2791db9b54c397178f8a62373fe37f6fd5d4217a
Derick Bailey <derickbailey@gmail.com>
2010-05-11 10:52:11 -0500

    working on csc spec a tiny bit

--- lib/albacore/csc.rb
+++ lib/albacore/csc.rb
@@ -1,4 +1,14 @@
 require 'albacore/support/albacore_helper'
 
 class CSC
+  extend AttrMethods
+  include RunCommand
+  include YAMLConfig
+  include Logging
+
+  attr_array :compile
+
+  def execute
+  end
+
 end
--- spec/csc_spec.rb
+++ spec/csc_spec.rb
@@ -1,14 +1,18 @@
 require File.join(File.dirname(__FILE__), 'support', 'spec_helper')
 require 'albacore/csc'
 
-describe CSC, "when supplying a file list to compile" do
+describe CSC, "when supplying a file list with 2 files to compile" do
   before :each do
     csc = CSC.new
     csc.compile FileList["File1.cs", "File2.cs"]
+    csc.execute
   end
 
-  it "should provide the file list to the compiler" do
+  it "should provide the first file to the compiler" do
     @system_command.should contain("\"File1.cs\"")
+  end
+
+  it "should provide the second file to the compiler" do
     @system_command.should contain("\"File2.cs\"")
   end
 end
