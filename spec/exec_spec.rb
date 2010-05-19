d82c0421afc76a9e630133adde0697b651324d15
Derick Bailey <derickbailey@gmail.com>
2010-05-18 19:58:28 -0500

    fixed exec spec

--- spec/exec_spec.rb
+++ spec/exec_spec.rb
@@ -1,21 +1,21 @@
 require File.join(File.dirname(__FILE__), 'support', 'spec_helper')
 require 'albacore/exec'
 
-@@nunit = File.join(File.dirname(__FILE__), 'support', 'Tools', 'NUnit-v2.5', 'nunit-console-x86.exe')
-
 describe Exec, "when executing a command with parameters" do
   before :all do
+    @nunit = File.join(File.dirname(__FILE__), 'support', 'Tools', 'NUnit-v2.5', 'nunit-console-x86.exe')
+
     @cmd = Exec.new
     @cmd.log_level = :verbose
     @cmd.extend(SystemPatch)
     @cmd.extend(FailPatch)
-    @cmd.path_to_command = @@nunit
+    @cmd.path_to_command = @nunit
     @cmd.parameters "--help"
     @cmd.execute
   end
   
   it "should run the command with the parameters" do
-    @cmd.system_command.should include("\"#{@@nunit}\" --help")
+    @cmd.system_command.should include("\"#{@nunit}\" --help")
   end
   
   it "should specify the parameters only once" do
