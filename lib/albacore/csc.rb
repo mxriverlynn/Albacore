4a1daaa3b22a5b1735803b809eb694fa799048a8
Derick Bailey <derickbailey@gmail.com>
2010-05-30 10:34:20 -0500

    starting to separate config from runtime

--- lib/albacore/csc.rb
+++ lib/albacore/csc.rb
@@ -1,14 +1,30 @@
 require 'albacore/support/albacore_helper'
 
-class CSC
+Albacore.configure do |config|
+  config.add_path :csc, ""
+end
+
+class CSCConfig
   extend AttrMethods
-  include RunCommand
   include YAMLConfig
   include Logging
 
   attr_array :compile
 
-  def execute
+  def initialize
+    command = :csc
+    super()
   end
+end
 
+class CSC
+  include RunCommand
+  include Logging
+
+  def execute(config)
+    result = run_command "CSC", command_parameters.join(" ")
+    
+    failure_message = 'CSC Failed. See Build Log For Detail'
+    fail_with_message failure_message if !result
+  end
 end
