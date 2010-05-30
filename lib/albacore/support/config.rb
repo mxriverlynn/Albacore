5cedddd8e48d40a163666b7e5db83be92c5fd934
Derick Bailey <derickbailey@gmail.com>
2010-05-30 09:30:37 -0500

    reducing the albacore.configure code back down to just paths for now. adding basic watchr script to help with continuous testing

--- lib/albacore/support/config.rb
+++ lib/albacore/support/config.rb
@@ -35,45 +35,5 @@ module Albacore
     def get_path(name)
       @paths[name]
     end
-
-    def add_command(name, path, command)
-      @commands[name] = Command.new path, command
-    end
-
-    def get_command(name)
-      cmd = @commands[name]
-      msg = "The '#{name.to_s}' task has not been configured"
-      fail msg if cmd.nil?
-      cmd.fullpath 
-    end
-
-    def has_command?(name)
-      @commands.include? name
-    end
-
-    def method_missing(symbol, *args)
-      case args.count
-        when 1
-          add_command symbol, nil, args[0]
-        when 2
-          add_command symbol, args[0], args[1]
-        else
-          super symbol, *args
-      end
-    end
-  end #Configuration
-
-  class Command
-    def initialize(path, command)
-      @path = path
-      @command = command
-    end
-
-    def fullpath
-      return @command if @path.nil?
-
-      path = Albacore.configuration.get_path(@path) || @path
-      File.join(path, @command)
-    end
   end
 end
--- spec/config_spec.rb
+++ spec/config_spec.rb
@@ -8,7 +8,7 @@ describe "when adding a new path with a supplied name" do
   end
   
   it "should make the path available by the supplied name" do
-    Albacore.configuration.get_path :path_name
+    Albacore.configuration.get_path(:path_name).should == "./whatever/"
   end
 end
 
@@ -28,81 +28,5 @@ end
 describe "when retrieving a path that does not exist" do
   it "should return nil" do 
     Albacore.configuration.get_path(:doesnotexist).should == nil
-  end
-end
-
-describe "when requesting the command for a task" do
-  before :each do
-    Albacore.configure do |config|
-      config.sometask "some/dir", "command.exe"
-    end
-  end
-
-  it "should provide the appended path and command" do
-    Albacore.configuration.get_command(:sometask).should == "some/dir/command.exe"
-  end
-end
-
-describe "when requesting the command for a task that was configured with a named path" do
-  before :each do
-    Albacore.configure do |config|
-      config.add_path :path, "what/ever"
-      config.mytask :path, "my.exe"
-    end
-  end
-
-  it "should evaluate the path by the name and append the path and command" do
-    Albacore.configuration.get_command(:mytask).should == "what/ever/my.exe"
-  end
-end
-
-describe "when requesting the command for a task that has not been registered" do
-  before :each do
-    begin
-      Albacore.configuration.get_command :has_not_been_configured
-    rescue
-      @error_message = $!
-    end
-  end
-  
-  it "should fail saying the task is not configured" do
-    @error_message.to_s.should == "The 'has_not_been_configured' task has not been configured"
-  end
-end
-
-describe "when registering a task with only a command parameter" do
-  before :each do
-    Albacore.configure do |config|
-      config.oneparam "one/parameter.exe"
-    end
-  end
-
-  it "should use the command parameter as the path and command" do
-    Albacore.configuration.get_command(:oneparam).should == "one/parameter.exe"
-  end
-end
-
-describe "when providing a configuration method to the configuration api" do
-  before :all do
-    class Test
-      attr_accessor :test
-    end
-
-    Albacore.configure do |config|
-      config.add_configuration :testfoo do |&block| 
-        block.call(Test.new) unless block.nil?
-      end
-    end
-
-    Albacore.configure do |config|
-      config.testfoo do |testdata|
-        testdata.test = "this is config data"
-        @configdata = testdata
-      end
-    end
-  end
-
-  it "should accept a parameter for configuration data" do
-    @configdata.test.should == "this is config data"
   end
 end
