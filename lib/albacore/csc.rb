2d26935d05993a2dfbb87931e344aa1305b05f81
Louis Salin <louis.phil@gmail.com>
2010-06-20 22:14:13 -0500

    changed the CSC class to support a Linux environment by not changing forward slashes to backward slashes

--- lib/albacore/csc.rb
+++ lib/albacore/csc.rb
@@ -4,11 +4,34 @@ Albacore.configure do |config|
   config.add_path :csc, "C:\\Windows\\Microsoft.NET\\Framework\\v4.0.30319"
 end
 
+module SupportsLinuxEnvironment
+  attr_accessor :is_linux
+  
+  def initialize
+    @is_linux = RUBY_PLATFORM.include? 'linux'
+    super()
+  end
+  
+  def format_reference(reference)
+    "\"/reference:#{to_OS_format(reference)}\""
+  end
+
+  def format_path(path)
+    "\"#{to_OS_format(path)}\""
+  end
+
+  def to_OS_format(input)
+    formatted_input = @is_linux ? input : input.gsub("/", "\\")
+	formatted_input
+  end
+end
+
 class CSC
   extend AttrMethods
   include YAMLConfig
   include RunCommand
   include Logging
+  include SupportsLinuxEnvironment
 
   attr_accessor :output, :target
   attr_array :compile, :references
@@ -22,8 +45,8 @@ class CSC
     params = []
     params << "\"/out:#{@output}\"" unless @output.nil?
     params << "/target:#{@target}" unless @target.nil?
-    params << @references.map{|r| "\"/reference:#{r.gsub("/","\\")}\""} unless @references.nil?
-    params << @compile.map{|f| "\"#{f}\"".gsub("/", "\\")} unless @compile.nil?
+    params << @references.map{|r| format_reference(r)} unless @references.nil?
+    params << @compile.map{|f| format_path(f)} unless @compile.nil?
 
     result = run_command "CSC", params
     
