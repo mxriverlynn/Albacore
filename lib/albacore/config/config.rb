202982b9a82e40486875f0783172ee7930f2e59e
Derick Bailey <derickbailey@gmail.com>
2010-08-05 12:49:59 -0500

    minor cleanup for config

--- lib/albacore/config/config.rb
+++ lib/albacore/config/config.rb
@@ -1,22 +1,25 @@
 module Albacore
   class << self
     def configure
+      yield(configuration) if block_given?
+      configuration
+    end
+
+    def configuration
       @configuration ||= Configuration.new
-      yield(@configuration) if block_given?
-      @configuration
     end
   end
 
   class Configuration
-   	attr_accessor :yaml_config_folder, :log_level, :plugindir
+   	attr_accessor :yaml_config_folder, :log_level
+    attr_reader :plugindir
 
     def initialize
       @plugindir = File.expand_path(File.join(Dir.pwd, "albacore"))
-      super()
     end
   end
 
-  Dir.glob(File.join(Albacore.configure.plugindir, "*.rb")).each do |f| 
+  Dir.glob(File.join(Albacore.configuration.plugindir, "*.rb")).each do |f| 
     require f 
   end
 end
