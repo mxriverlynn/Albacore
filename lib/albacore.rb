bb3570ed1f2e3792b41a83a8d7ca3d9edd6f9035
Derick Bailey <derickbailey@gmail.com>
2010-08-05 12:30:00 -0500

    cleaning up the albacore.rb file

--- lib/albacore.rb
+++ lib/albacore.rb
@@ -1,15 +1,16 @@
-$: << File.expand_path(File.dirname(__FILE__))
-$: << File.expand_path(File.join(File.dirname(__FILE__), "albacore"))
-$: << File.expand_path(File.join(File.dirname(__FILE__), "albacore", 'support'))
-$: << File.expand_path(File.join(File.dirname(__FILE__), "albacore", 'config'))
-$: << File.expand_path(File.join(File.dirname(__FILE__), "rake"))
+albacore_root = File.expand_path(File.dirname(__FILE__))
+$: << albacore_root
+$: << File.join(albacore_root, "albacore")
+$: << File.join(albacore_root, "albacore", 'support')
+$: << File.join(albacore_root, "albacore", 'config')
+$: << File.join(albacore_root, "rake")
 
 require 'logging'
 
 runtime_is_ironruby = (!defined?(IRONRUBY_VERSION).nil?)
 
-Dir.glob(File.join(File.expand_path(File.dirname(__FILE__)), 'albacore/*.rb')).each {|f| require f }
+Dir.glob(File.join(albacore_root, 'albacore/*.rb')).each {|f| require f }
 
-Dir.glob(File.join(File.expand_path(File.dirname(__FILE__)), 'rake/support/*.rb')).each {|f| require f }
+Dir.glob(File.join(albacore_root, 'rake/support/*.rb')).each {|f| require f }
 
-Dir.glob(File.join(File.expand_path(File.dirname(__FILE__)), 'rake/*.rb')).each {|f| require f }
+Dir.glob(File.join(albacore_root, 'rake/*.rb')).each {|f| require f }
