779101ed1b1f452142623c39d802b4231d8c502f
Henrik <henrik@haf.se>
2011-10-24 21:28:34 +0200

    shuffled the large asm per-setting test into a single loop with hash that varies what is to be asserted

--- spec/assemblyinfo_spec.rb
+++ spec/assemblyinfo_spec.rb
@@ -204,204 +204,81 @@ describe AssemblyInfo, "when specifying an attribute with non-string data" do
   end
 end
 
-describe AssemblyInfo, "when generating an assembly info file with the built in attributes and no language specified" do
+{ :no => { :engine => nil,              :lang => "no", :start_token => "[", :end_token => "]",     :using => "using " },
+  :cs => { :engine => CSharpEngine.new, :lang => "C#", :start_token => "[", :end_token => "]",     :using => "using " },
+  :vb => { :engine => VbNetEngine.new,  :lang => "VB.Net", :start_token => "<", :end_token => ">", :using => "Imports " },
+  :fs => { :engine => FSharpEngine.new, :lang => "F#", :start_token => "[<", :end_token => ">]",   :using => "open " }
+}.each do |key, settings|
 
-  include_context "asminfo task"
+  describe AssemblyInfo, "when generating an assembly info file with the built in attributes and #{settings[:lang]} language specified" do
 
-  before :all do
-    @asm.product_name = @tester.product_name
-    @asm.version = @tester.version
-    @asm.title = @tester.title
-    @asm.description = @tester.description
-    @asm.copyright = @tester.copyright
-    @asm.com_visible = @tester.com_visible
-    @asm.com_guid = @tester.com_guid
-    @asm.file_version = @tester.file_version
-    @asm.trademark = @tester.trademark
-    @asm.company_name = @tester.company_name
-  end
+    include_context "language engines"
 
-  subject { @tester.build_and_read_assemblyinfo_file @asm }
+    before :all do
+      @asm.product_name = @tester.product_name
+      @asm.version = @tester.version
+      @asm.title = @tester.title
+      @asm.description = @tester.description
+      @asm.copyright = @tester.copyright
+      @asm.com_visible = @tester.com_visible
+      @asm.com_guid = @tester.com_guid
+      @asm.file_version = @tester.file_version
+      @asm.trademark = @tester.trademark
+      @asm.company_name = @tester.company_name
 
-  it "should use the system.reflection namespace" do
-    subject.scan('using System.Reflection;').length.should == 1
-  end
-  
-  it "should use the system.runtime.interopservices namespace" do
-    subject.scan('using System.Runtime.InteropServices;').length.should == 1
-  end
-  
-  it "should contain the specified version information" do
-    subject.scan(%Q|[assembly: AssemblyVersion("#{@tester.version}")]|).length.should == 1
-  end
-  
-  it "should contain the assembly title" do
-    subject.scan(%Q|[assembly: AssemblyTitle("#{@tester.title}")]|).length.should == 1
-  end
-  
-  it "should contain the assembly description" do
-    subject.scan(%Q|[assembly: AssemblyDescription("#{@tester.description}")|).length.should == 1
-  end
-  
-  it "should contain the copyright information" do
-    subject.scan(%Q|[assembly: AssemblyCopyright("#{@tester.copyright}")]|).length.should == 1
-  end
-  
-  it "should contain the com visible information" do
-    subject.scan(%Q|[assembly: ComVisible(#{@tester.com_visible})]|).length.should == 1
-    subject.scan(%Q|[assembly: Guid("#{@tester.com_guid}")]|).length.should == 1
-  end
-  
-  it "should contain the company name information" do
-    subject.scan(%Q|[assembly: AssemblyCompany("#{@tester.company_name}")]|).length.should == 1
-  end
-  
-  it "should contain the product information" do
-    subject.scan(%Q|[assembly: AssemblyProduct("#{@tester.product_name}")]|).length.should == 1
-  end
-  
-  it "should contain the file version information" do
-    subject.scan(%Q|[assembly: AssemblyFileVersion("#{@tester.file_version}")]|).length.should == 1
-  end
-  
-  it "should contain the trademark information" do
-    subject.scan(%Q|[assembly: AssemblyTrademark("#{@tester.trademark}")]|).length.should == 1
-  end
-end
+      using_engine settings[:engine] unless settings[:engine].nil?
+    end
 
-describe AssemblyInfo, "when generating an assembly info file with the built in attributes and C# specified" do
-  before :all do
-    @tester = AssemblyInfoTester.new
-    @tester.lang_engine = CSharpEngine.new
-    asm = AssemblyInfo.new
-    asm.lang_engine = CSharpEngine.new
-    
-    asm.company_name = @tester.company_name
-    asm.product_name = @tester.product_name
-    asm.version = @tester.version
-    asm.title = @tester.title
-    asm.description = @tester.description
-    asm.copyright = @tester.copyright
-    asm.com_visible = @tester.com_visible
-    asm.com_guid = @tester.com_guid
-    asm.file_version = @tester.file_version
-    asm.trademark = @tester.trademark
-    
-    # Generate the same file twice.
-    @tester.build_and_read_assemblyinfo_file asm
-    @filedata = @tester.build_and_read_assemblyinfo_file asm
-  end
-  
-  it "should use the system.reflection namespace" do
-    @filedata.scan('using System.Reflection;').length.should == 1
-  end
-  
-  it "should use the system.runtime.interopservices namespace" do
-    @filedata.scan('using System.Runtime.InteropServices;').length.should == 1
-  end
-  
-  it "should contain the specified version information" do
-    @filedata.scan(%Q|[assembly: AssemblyVersion("#{@tester.version}")]|).length.should == 1
-  end
-  
-  it "should contain the assembly title" do
-    @filedata.scan(%Q|[assembly: AssemblyTitle("#{@tester.title}")]|).length.should == 1
-  end
-  
-  it "should contain the assembly description" do
-    @filedata.scan(%Q|[assembly: AssemblyDescription("#{@tester.description}")]|).length.should == 1
-  end
-  
-  it "should contain the copyright information" do
-    @filedata.scan(%Q|[assembly: AssemblyCopyright("#{@tester.copyright}")]|).length.should == 1
-  end
-  
-  it "should contain the com visible information" do
-    @filedata.scan(%Q|[assembly: ComVisible(#{@tester.com_visible})]|).length.should == 1
-    @filedata.scan(%Q|[assembly: Guid("#{@tester.com_guid}")]|).length.should == 1
-  end
-  
-  it "should contain the company name information" do
-    @filedata.scan(%Q|[assembly: AssemblyCompany("#{@tester.company_name}")]|).length.should == 1
-  end
-  
-  it "should contain the product information" do
-    @filedata.scan(%Q|[assembly: AssemblyProduct("#{@tester.product_name}")]|).length.should == 1
-  end
-  
-  it "should contain the file version information" do
-    @filedata.scan(%Q|[assembly: AssemblyFileVersion("#{@tester.file_version}")]|).length.should == 1
-  end
-  
-  it "should contain the trademark information" do
-    @filedata.scan(%Q|[assembly: AssemblyTrademark("#{@tester.trademark}")]|).length.should == 1
-  end
-end
+    let(:s) { settings[:start_token] }
+    let(:e) { settings[:end_token] }
+    let(:using) { settings[:using] }
 
-describe VbNetEngine, "when generating an assembly info file with the built in attributes and VB.NET specified" do
+    subject { @tester.build_and_read_assemblyinfo_file @asm }
 
-  include_context "language engines"
+    it "should use the system.reflection namespace" do
+      subject.scan(%Q|#{using}System.Reflection|).length.should == 1
+    end
 
-  before :all do
-    @asm.company_name = @tester.company_name
-    @asm.product_name = @tester.product_name
-    @asm.version = @tester.version
-    @asm.title = @tester.title
-    @asm.description = @tester.description
-    @asm.copyright = @tester.copyright
-    @asm.com_visible = @tester.com_visible
-    @asm.com_guid = @tester.com_guid
-    @asm.file_version = @tester.file_version
-    @asm.trademark = @tester.trademark
+    it "should use the system.runtime.interopservices namespace" do
+      subject.scan(%Q|#{using}System.Runtime.InteropServices|).length.should == 1
+    end
 
-    using_engine VbNetEngine.new
-  end
+    it "should contain the specified version information" do
+      subject.scan(%Q|#{s}assembly: AssemblyVersion("#{@tester.version}")#{e}|).length.should == 1
+    end
 
-  subject { @tester.build_and_read_assemblyinfo_file @asm }
+    it "should contain the assembly title" do
+      subject.scan(%Q|#{s}assembly: AssemblyTitle("#{@tester.title}")#{e}|).length.should == 1
+    end
 
-  it "should use the system.reflection namespace" do
-    subject.scan('Imports System.Reflection').length.should == 1
-  end
-  
-  it "should use the system.runtime.interopservices namespace" do
-    subject.scan('Imports System.Runtime.InteropServices').length.should == 1
-  end
-  
-  it "should contain the specified version information" do
-    subject.scan(%Q|<assembly: AssemblyVersion("#{@tester.version}")>|).length.should == 1
-  end
-  
-  it "should contain the assembly title" do
-    subject.scan(%Q|<assembly: AssemblyTitle("#{@tester.title}")>|).length.should == 1
-  end
-  
-  it "should contain the assembly description" do
-    subject.scan(%Q|<assembly: AssemblyDescription("#{@tester.description}")>|).length.should == 1
-  end
-  
-  it "should contain the copyright information" do
-    subject.scan(%Q|<assembly: AssemblyCopyright("#{@tester.copyright}")>|).length.should == 1
-  end
-  
-  it "should contain the com visible information" do
-    subject.scan(%Q|<assembly: ComVisible(#{@tester.com_visible})>|).length.should == 1
-    subject.scan(%Q|<assembly: Guid("#{@tester.com_guid}")>|).length.should == 1
-  end
-  
-  it "should contain the company name information" do
-    subject.scan(%Q|<assembly: AssemblyCompany("#{@tester.company_name}")>|).length.should == 1
-  end
-  
-  it "should contain the product information" do
-    subject.scan(%Q|<assembly: AssemblyProduct("#{@tester.product_name}")>|).length.should == 1
-  end
-  
-  it "should contain the file version information" do
-    subject.scan(%Q|<assembly: AssemblyFileVersion("#{@tester.file_version}")>|).length.should == 1
-  end
-  
-  it "should contain the trademark information" do
-    subject.scan(%Q|<assembly: AssemblyTrademark("#{@tester.trademark}")>|).length.should == 1
+    it "should contain the assembly description" do
+      subject.scan(%Q|#{s}assembly: AssemblyDescription("#{@tester.description}")#{e}|).length.should == 1
+    end
+
+    it "should contain the copyright information" do
+      subject.scan(%Q|#{s}assembly: AssemblyCopyright("#{@tester.copyright}")#{e}|).length.should == 1
+    end
+
+    it "should contain the com visible information" do
+      subject.scan(%Q|#{s}assembly: ComVisible(#{@tester.com_visible})#{e}|).length.should == 1
+      subject.scan(%Q|#{s}assembly: Guid("#{@tester.com_guid}")#{e}|).length.should == 1
+    end
+
+    it "should contain the company name information" do
+      subject.scan(%Q|#{s}assembly: AssemblyCompany("#{@tester.company_name}")#{e}|).length.should == 1
+    end
+
+    it "should contain the product information" do
+      subject.scan(%Q|#{s}assembly: AssemblyProduct("#{@tester.product_name}")#{e}|).length.should == 1
+    end
+
+    it "should contain the file version information" do
+      subject.scan(%Q|#{s}assembly: AssemblyFileVersion("#{@tester.file_version}")#{e}|).length.should == 1
+    end
+
+    it "should contain the trademark information" do
+      subject.scan(%Q|#{s}assembly: AssemblyTrademark("#{@tester.trademark}")#{e}|).length.should == 1
+    end
   end
 end
 
