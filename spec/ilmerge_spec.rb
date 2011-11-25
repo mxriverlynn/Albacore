8a516a98ad0f96c5b7029085ca41618be45f7cf3
tkellogg <tkellogg@kpaonline.com>
2011-11-25 14:46:32 -0700

    One test passes, the other is still failing

--- spec/ilmerge_spec.rb
+++ spec/ilmerge_spec.rb
@@ -19,25 +19,26 @@ describe Albacore::IlMergeResolver, "Global Albacore#configure configuration" do
 	describe "when Albacore::ConfigData#ilmerge_path hasn't been set" do
 
 		context "and ilmerge.exe exists in Program Files" do
-			before :all do
-				@il_path = 'C:\Program Files\Microsoft\ILMerge\ilmerge.exe'
+			before :each do
+				@il_path = %q{C:\Program Files\Microsoft\ILMerge\ilmerge.exe}
 				set_file_exists @il_path, true
 				@me = Albacore::IlMergeResolver.new ''
 			end
 
 			subject { @me.resolve }
-			it { should be @il_path }
+			it { should == @il_path }
 		end
 
 		context "and ilmerge only exists in Program Files (x86)" do
 			before :all do
-				@il_path = 'C:\Program Files\Microsoft\ILMerge\ilmerge.exe'
+				@il_path = %q{C:\Program Files (x86)\Microsoft\ILMerge\ilmerge.exe}
+				set_file_exists %q{C:\Program Files\Microsoft\ILMerge\ilmerge.exe}, false 
 				set_file_exists @il_path, true
 				@me = Albacore::IlMergeResolver.new ''
 			end
 
 			subject { @me.resolve }
-			it { should be @il_path }
+			it { should == @il_path }
 		end
 	end
 
