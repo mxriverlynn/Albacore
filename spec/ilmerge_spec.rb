bd64bd1b17f20cadfb04963a519790b1792d0f02
tkellogg <tkellogg@kpaonline.com>
2011-11-25 17:13:24 -0700

    refactored tests to reduce duplication

--- spec/ilmerge_spec.rb
+++ spec/ilmerge_spec.rb
@@ -20,9 +20,8 @@ describe Albacore::IlMergeResolver, "Global Albacore#configure configuration" do
 
 		context "and ilmerge.exe exists in Program Files" do
 			before :each do
+				set_file_exists '' => true, ' (x86)' => false
 				@il_path = %q{C:\Program Files\Microsoft\ILMerge\ilmerge.exe}
-				set_file_exists %q{C:\Program Files (x86)\Microsoft\ILMerge\ilmerge.exe}, false 
-				set_file_exists @il_path, true
 				@me = Albacore::IlMergeResolver.new ''
 			end
 
@@ -32,9 +31,8 @@ describe Albacore::IlMergeResolver, "Global Albacore#configure configuration" do
 
 		context "and ilmerge only exists in Program Files (x86)" do
 			before :all do
+				set_file_exists '' => false, ' (x86)' => true
 				@il_path = %q{C:\Program Files (x86)\Microsoft\ILMerge\ilmerge.exe}
-				set_file_exists %q{C:\Program Files\Microsoft\ILMerge\ilmerge.exe}, false 
-				set_file_exists @il_path, true
 				@me = Albacore::IlMergeResolver.new ''
 			end
 
@@ -43,8 +41,11 @@ describe Albacore::IlMergeResolver, "Global Albacore#configure configuration" do
 		end
 	end
 
-	def set_file_exists(path, exists)
+	def set_file_exists(items)
+		items.each_pair do |path,exists| 
+			path = "C:\\Program Files#{path}\\Microsoft\\ILMerge\\ilmerge.exe"
 			File.should_receive(:exists?).with(path).and_return(exists)
+		end
 	end
 
 end
