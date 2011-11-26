95ae4061e5128afef2f332a5a3baaca30993fe2c
tkellogg <tkellogg@kpaonline.com>
2011-11-25 17:31:45 -0700

    Allow for explictly specified path to ilmerge

--- spec/ilmerge_spec.rb
+++ spec/ilmerge_spec.rb
@@ -16,13 +16,13 @@ end
 
 describe Albacore::IlMergeResolver, "Global Albacore#configure configuration" do
 
-	describe "when Albacore::ConfigData#ilmerge_path hasn't been set" do
+	describe "when ilmerge_path hasn't been set" do
 
 		context "and ilmerge.exe exists in Program Files" do
 			before :each do
 				set_file_exists '' => true, ' (x86)' => false
 				@il_path = %q{C:\Program Files\Microsoft\ILMerge\ilmerge.exe}
-				@me = Albacore::IlMergeResolver.new ''
+				@me = Albacore::IlMergeResolver.new 
 			end
 
 			subject { @me.resolve }
@@ -33,12 +33,21 @@ describe Albacore::IlMergeResolver, "Global Albacore#configure configuration" do
 			before :all do
 				set_file_exists '' => false, ' (x86)' => true
 				@il_path = %q{C:\Program Files (x86)\Microsoft\ILMerge\ilmerge.exe}
-				@me = Albacore::IlMergeResolver.new ''
+				@me = Albacore::IlMergeResolver.new 
 			end
 
 			subject { @me.resolve }
 			it { should == @il_path }
 		end
+	end
+
+	context "when ilmerge_path has been set" do
+		before :each do
+			@me = Albacore::IlMergeResolver.new 'path that exists'
+		end
+
+		subject { @me.resolve }
+		it { should == 'path that exists' }
 	end
 
 	def set_file_exists(items)
