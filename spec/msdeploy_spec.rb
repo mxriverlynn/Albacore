2e8c04b220dc60b02b5a93358c820426c300a0a2
Bluephlame <justin@boyd.net.au>
2012-02-12 22:32:54 +1100

    adding spec tests for MSDeploy task

---
+++ spec/msdeploy_spec.rb
@@ -0,0 +1,108 @@
+require 'spec_helper'
+require 'albacore/msdeploy'
+
+describe MSDeploy, "Deploying a package without specifying anything" do
+  before :each do
+    @msdeploy = MSDeploy.new
+    @msdeploy.extend(SystemPatch)
+  end
+  
+  it "Should raise excecption as package is not in the CWD" do
+    lambda {@msdeploy.execute}.should raise_error(RuntimeError ,"Could not find the MSDeploy package to deploy.")
+  end
+end
+
+describe MSDeploy, "Deploying a package from a folder" do
+  
+  before :each do
+    @msdeploy = MSDeploy.new
+    @msdeploy.deploy_package = "spec\\support\\TestSolution\\TestSolution.MSDeploy\\TestSolution.MSDeploy\\obj\\Release\\Package"
+    @msdeploy.noop = true
+    @msdeploy.extend(SystemPatch)
+  end
+  
+  it "should deploy locally" do
+    lambda {@msdeploy.execute}.should_not raise_error(RuntimeError ,"MSDeploy Failed.  See build log for details")  
+    @msdeploy.system_command.scan(/-whatif/).length.should be(1)
+  end
+  
+  it "with username, password and server, should fail as that is not a valid username/password or server" do
+    @msdeploy.username = "testname"
+    @msdeploy.password = "testpasssword"
+    @msdeploy.server = "testserver"
+    #assert
+    lambda {@msdeploy.execute}.should raise_error(RuntimeError ,"MSDeploy Failed.  See build log for details")
+    @msdeploy.system_command.scan(/computerName='testserver',userName='testname',password='testpasssword'/).length.should be(1)
+    end
+end
+
+describe MSDeploy, "Deploying a package from a zip" do
+  before :each do
+    @msdeploy = MSDeploy.new
+    @msdeploy.extend(SystemPatch)
+    @msdeploy.noop = true
+    @msdeploy.deploy_package = "spec/support/TestSolution/TestSolution.MSDeploy/TestSolution.MSDeploy/obj/Debug/Package"
+  end
+  
+   it "should deploy locally" do
+    lambda {@msdeploy.execute}.should_not raise_error(RuntimeError ,"MSDeploy Failed.  See build log for details")  
+    @msdeploy.system_command.scan(/-whatif/).length.should be(1)    
+  end
+  
+  it "with username, password and server, should fail as that is not a valid username/password or server" do
+    @msdeploy.username = "testname"
+    @msdeploy.password = "testpasssword"
+    @msdeploy.server = "testserver"
+    #assert
+    lambda {@msdeploy.execute}.should raise_error(RuntimeError ,"MSDeploy Failed.  See build log for details")
+    @msdeploy.system_command.scan(/computerName='testserver',userName='testname',password='testpasssword'/).length.should be(1)
+  end
+    
+    it "with aditional parameters" do
+    @msdeploy.additional_parameters = "-usechecksum"
+    lambda {@msdeploy.execute}.should_not raise_error(RuntimeError ,"MSDeploy Failed.  See build log for details")  
+    @msdeploy.system_command.scan(/-usechecksum/).length.should be(1)   
+  end
+  
+  it "with specifying a parameter file" do
+    @msdeploy.parameters_file = "spec/support/TestSolution/TestSolution.MSDeploy/TestSolution.MSDeploy/obj/Debug/Package/TestSolution.MSDeploy.SetParameters.xml"
+    lambda {@msdeploy.execute}.should_not raise_error(RuntimeError ,"MSDeploy Failed.  See build log for details")  
+    @msdeploy.system_command.scan(/-setParamFile:/).length.should be(1)
+    @msdeploy.system_command.scan(/TestSolution.MSDeploy.SetParameters.xml/).length.should be(1)
+  end  
+  
+  it "With specifying a bad parameter file, will throw an exception" do
+      @msdeploy.parameters_file = "spec/support/TestSolution/TestSolution.MSDeploy/TestSolution.MSDeploy/TestSolution.MSDeploy.SetParameters.xml"
+    lambda {@msdeploy.execute}.should raise_error(RuntimeError ,"Could not find parameter file specified.")      
+  end
+  
+  
+end
+
+
+
+=begin
+desc "This is my Task"
+msdeploy :msdeploy do |msdeploy|
+  puts "Msdeploy task"
+  msdeploy.package = "spec/support/TestSolution/TestSolution.MSDeploy/TestSolution.MSDeploy/obj/Debug/Package"
+  msdeploy.noop = true
+end
+
+desc "This is my Task"
+msdeploy :msdeploy2 do |msdeploy|
+  puts "Msdeploy 2 task"
+  msdeploy.package = "spec/support/TestSolution/TestSolution.MSDeploy/TestSolution.MSDeploy/obj/Release/Package"
+  msdeploy.noop = true
+end
+
+desc "This is my Task"
+msdeploy :msdeploy3 do |msdeploy|
+  puts "Msdeploy 3 task"
+  msdeploy.package = "spec/support/TestSolution/TestSolution.MSDeploy/TestSolution.MSDeploy/obj/Release/Package"
+  msdeploy.username="testusername"
+  msdeploy.server="testserver"
+  msdeploy.password="testpassword"
+  msdeploy.noop = true
+end
+=end
