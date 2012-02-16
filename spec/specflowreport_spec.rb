fc134eb54c3d76fda382495f7cfab2cc90616efe
Chris Chandler <chris@developingchris.com>
2012-02-16 17:52:27 -0500

    updated format of this report for new rspec

--- spec/specflowreport_spec.rb
+++ spec/specflowreport_spec.rb
@@ -2,132 +2,129 @@ require 'spec_helper'
 require 'albacore/specflowreport'
 require 'albacore/nunittestrunner'
 
-shared_examples_for "specflow paths" do
+describe "specflow paths" do
   before :all do
     @specflowpath = File.join(File.dirname(__FILE__), 'support', 'Tools', 'SpecFlow', 'specflow.exe')
     @test_project = File.join(File.expand_path(File.dirname(__FILE__)), 'support', 'TestSolution', 'TestSolution.SpecFlow', 'TestSolution.SpecFlow.csproj')
     @nunit_test_restuls = "/xmlTestResult:TestResult.xml"
     @output_option = "/out:specs.html"
   end
-end
 
-describe SpecFlowReport, "when running without specifying command path" do  
-  it_should_behave_like "specflow paths"
-	
-  before :all do
-    @spec = SpecFlowReport.new()
-    @spec.projects @test_project
+  describe SpecFlowReport, "when running without specifying command path" do  
+
+    before :all do
+      @spec = SpecFlowReport.new()
+      @spec.projects @test_project
+    end
+
+    it "should try to run in same folder" do
+      @spec.command.should eql('specflow.exe')
+    end
   end
 
-  it "should try to run in same folder" do
-    @spec.command.should eql('specflow.exe')
+  describe SpecFlowReport, "When passing a command" do
+
+    before :all do
+      @spec = SpecFlowReport.new("/path_to_command/")
+      @spec.projects @test_project
+    end
+
+    it "should not include specflow.exe" do
+      @spec.command.should eql('/path_to_command/')
+    end
+  end
+
+  describe SpecFlowReport, "When passing some options" do
+    before :all do
+      spec =  SpecFlowReport.new()
+      spec.options @output_option
+      spec.projects @test_project
+      @command_line = spec.get_command_line
+    end
+
+    it "should include the options in the command line" do
+      @command_line.should include(@output_option)
+    end
+  end
+
+  describe SpecFlowReport, "When no options are passed" do
+
+    before :all do
+      spec = SpecFlowReport.new()
+      spec.projects @test_project
+      @command_line = spec.get_command_line
+    end
+
+    it "should include sensible defaults" do
+      @command_line.should include("/xmlTestResult:TestResult.xml /out:specs.html")
+    end
+  end
+
+  describe SpecFlowReport, "When a report is specified" do
+    before :all do
+      @spec = SpecFlowReport.new()
+      @spec.report = 'assigned report'
+      @spec.projects @test_project
+    end
+
+    it "should include the specified report in the command line" do
+      @spec.get_command_line.should include("assigned report")
+    end
+  end
+
+  describe SpecFlowReport, "When no report is specified" do
+    before :all do
+      @spec = SpecFlowReport.new()
+      @spec.projects @test_project
+    end
+
+    it "should include the nunit report in the command line" do
+      @spec.get_command_line.should include("nunitexecutionreport")
+    end
+  end
+
+  describe SpecFlowReport, "when configured correctly" do
+
+    before :all do
+      nunitpath = File.join(File.dirname(__FILE__), 'support', 'Tools', 'NUnit-v2.5', 'nunit-console-x86.exe')
+      test_assembly = File.join(File.expand_path(File.dirname(__FILE__)), 'support', 'SpecFlow', 'TestSolution.SpecFlow.dll')
+
+      nunit = NUnitTestRunner.new(nunitpath)
+      nunit.extend(FailPatch)
+      nunit.assemblies test_assembly
+      nunit.options '/noshadow /out=TestResult.xml'
+      nunit.execute
+
+      spec = SpecFlowReport.new(@specflowpath)
+      spec.extend(FailPatch)
+      spec.projects @test_project
+
+      spec.execute
+    end
+
+    it "should execute" do
+      $task_failed.should be_false
+    end
+  end
+
+  describe SpecFlowReport, "When not passing a project" do
+
+    before :all do
+      @spec = SpecFlowReport.new()
+      @spec.extend(FailPatch)
+      @spec.execute
+      $task_failed.should be_true
+    end
+
+    it "should fail" do
+      $task_failed.should be_true
+    end
   end
 end
 
-describe SpecFlowReport, "When not passing a project" do
-	
-	before :all do
-		@spec = SpecFlowReport.new()
-	 	@spec.extend(FailPatch)
-    	@spec.execute
-    	$task_failed.should be_true
-	end
-	
-	it "should fail" do
-		$task_failed.should be_true
-	end
-end
 
-describe SpecFlowReport, "When passing a command" do
-	it_should_behave_like "specflow paths"
-	
-	before :all do
-		@spec = SpecFlowReport.new("/path_to_command/")
-		@spec.projects @test_project
-	end
-	
-	it "should not include specflow.exe" do
-		@spec.command.should eql('/path_to_command/')
-	end
-end
 
-describe SpecFlowReport, "When passing some options" do
-	it_should_behave_like "specflow paths"
-	before :all do
-		spec =  SpecFlowReport.new()
-		spec.options @output_option
-		spec.projects @test_project
-		@command_line = spec.get_command_line
-	end
-	
-	it "should include the options in the command line" do
-		@command_line.should include(@output_option)
-	end
-end
 
-describe SpecFlowReport, "When no options are passed" do
-	it_should_behave_like "specflow paths"
-	
-	before :all do
-		spec = SpecFlowReport.new()
-		spec.projects @test_project
-		@command_line = spec.get_command_line
-	end
-	
-	it "should include sensible defaults" do
-		@command_line.should include("/xmlTestResult:TestResult.xml /out:specs.html")
-	end
-end
-
-describe SpecFlowReport, "When a report is specified" do
-	it_should_behave_like "specflow paths"
-	before :all do
-		@spec = SpecFlowReport.new()
-		@spec.report = 'assigned report'
-		@spec.projects @test_project
-	end
-	
-	it "should include the specified report in the command line" do
-		@spec.get_command_line.should include("assigned report")
-	end
-end
-
-describe SpecFlowReport, "When no report is specified" do
-	it_should_behave_like "specflow paths"
-	before :all do
-		@spec = SpecFlowReport.new()
-		@spec.projects @test_project
-	end
-	
-	it "should include the nunit report in the command line" do
-		@spec.get_command_line.should include("nunitexecutionreport")
-	end
-end
-
-describe SpecFlowReport, "when configured correctly" do
-  it_should_behave_like "specflow paths"
-
-  before :all do
-  	nunitpath = File.join(File.dirname(__FILE__), 'support', 'Tools', 'NUnit-v2.5', 'nunit-console-x86.exe')
-    test_assembly = File.join(File.expand_path(File.dirname(__FILE__)), 'support', 'SpecFlow', 'TestSolution.SpecFlow.dll')
-
-  	nunit = NUnitTestRunner.new(nunitpath)
-    nunit.extend(FailPatch)
-    nunit.assemblies test_assembly
-    nunit.options '/noshadow /out=TestResult.xml'
-    nunit.execute
-    
-    spec = SpecFlowReport.new(@specflowpath)
-    spec.extend(FailPatch)
-    spec.projects @test_project
-    
-    spec.execute
-  end
-  
-  it "should execute" do
-    $task_failed.should be_false
-  end
-end
 
 describe SpecFlowReport, "when providing configuration" do
   let :specflow do
